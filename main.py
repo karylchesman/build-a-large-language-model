1ec632a187460547b10a8ff0d08e047eebb1ef2d
Karyl Chesman <karylbps@gmail.com>
2025-07-09 16:59:12 -0300

    feat: implemented a new tokenizer to handle unknown characters

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import re
-from simple_tokenizer_v1 import SimpleTokenizerV1
+from simple_tokenizer_v2 import SimpleTokenizerV2
 
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
@@ -7,16 +7,14 @@ with open("the-verdict.txt", "r", encoding="utf-8") as f:
 preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
 all_words = sorted(set(preprocessed))
+all_words.extend(["<|endoftext|>", "<|unk|>"])
 vocab_size = len(all_words)
 vocab = {token: integer for integer, token in enumerate(all_words)}
 
-tokenizer = SimpleTokenizerV1(vocab)
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
 
-text = """"It's the last he painted, you know," 
-       Mrs. Gisburn said with pardonable pride."""
-ids = tokenizer.encode(text)
-print(ids)
-print(tokenizer.decode(ids))
-
-text = "Hello, do you like tea?"
+tokenizer = SimpleTokenizerV2(vocab)
 print(tokenizer.encode(text))
+print(tokenizer.decode(tokenizer.encode(text)))
