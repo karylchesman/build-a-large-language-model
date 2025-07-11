78252e9b2f49559d4b9bd4840f2814dbd106c5bc
Karyl Chesman <karylbps@gmail.com>
2025-07-11 17:17:38 -0300

    feat: added a sliding window example to generate inputs

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import tiktoken
 
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
 
 tokenizer = tiktoken.get_encoding("gpt2")
-text = (
-    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
-    "of someunknownPlace."
-)
-integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
-print(integers)
 
-strings = tokenizer.decode(integers)
-print(strings)
+enc_text = tokenizer.encode(raw_text)
+enc_sample = enc_text[50:]
+
+context_size = 4
+
+for i in range(1, context_size + 1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
